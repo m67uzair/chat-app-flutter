5689fbc9e10d58d42289a7829b03717e6afd96b1
m67uzair <m67uzair@gmail.com>
2023-02-23 19:52:48 +0500

    chat provider done

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:chat_app_flutter/constants/firestore_constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -19,5 +20,50 @@ class ChatProvider {
   UploadTask uploadImageFile(File image, String filename) {
     Reference reference = firebaseStorage.ref().child(filename);
     UploadTask uploadTask = reference.putFile(image);
+    return uploadTask;
   }
+
+  Future<void> updateFirestoreData(
+      String collectionPath, String docpath, Map<String, dynamic> updatedData) {
+    return firebaseFirestore
+        .collection(collectionPath)
+        .doc(docpath)
+        .update(updatedData);
+  }
+
+  Stream<QuerySnapshot> getChatMessage(String groupChatId, int limit) {
+    return firebaseFirestore
+        .collection(FirestoreConstants.pathMessageCollection)
+        .doc(groupChatId)
+        .collection(groupChatId)
+        .orderBy(FirestoreConstants.timestamp, descending: true)
+        .limit(limit)
+        .snapshots();
+  }
+
+  void sendChatMessage(String content, int type, String groupChatId,
+      String currentUserId, String peerId) {
+    DocumentReference documentReference = firebaseFirestore
+        .collection(FirestoreConstants.pathMessageCollection)
+        .doc(groupChatId)
+        .collection(groupChatId)
+        .doc(DateTime.now().microsecondsSinceEpoch.toString());
+
+    ChatMessages chatMessages = ChatMessages(
+        idFrom: currentUserId,
+        idTo: peerId,
+        timestamp: DateTime.now().millisecondsSinceEpoch.toString(),
+        content: content,
+        type: type);
+
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      transaction.set(documentReference, chatMessages.toJason());
+    });
+  }
+}
+
+class MesageType {
+  static const text = 0;
+  static const image = 1;
+  static const sticker = 2;
 }
